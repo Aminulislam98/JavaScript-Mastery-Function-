4d1044dc17d13b0e66622bdb11a587b22f9a6721
Aminul islam <aminurrohman98@gmail.com>
2026-07-01 12:14:32 +0100

    deep dive into Default Parameters

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+// Default Parameters
+const bookings = [];
+const createBooking = function (
+  flightNum,
+  numPassengers = 1,
+  price = 199 * numPassengers,
+) {
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+console.log(bookings);
+createBooking('LH123', undefined, 800);
