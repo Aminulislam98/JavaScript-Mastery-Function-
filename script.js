821952a648a08c08280b2aba013a5a6c6447bd82
Aminul islam <aminurrohman98@gmail.com>
2026-07-01 13:38:57 +0100

    Deep dive How passing arguments works: value vs. reference

--- script.js
+++ script.js
@@ -1,19 +1,48 @@
 'use strict';
 
 // Default Parameters
-const bookings = [];
-const createBooking = function (
-  flightNum,
-  numPassengers = 1,
-  price = 199 * numPassengers,
-) {
-  const booking = {
-    flightNum,
-    numPassengers,
-    price,
-  };
-  console.log(booking);
-  bookings.push(booking);
+// const bookings = [];
+// const createBooking = function (
+//   flightNum,
+//   numPassengers = 1,
+//   price = 199 * numPassengers,
+// ) {
+//   const booking = {
+//     flightNum,
+//     numPassengers,
+//     price,
+//   };
+//   console.log(booking);
+//   bookings.push(booking);
+// };
+// console.log(bookings);
+// createBooking('LH123', undefined, 800);
+
+// How passing arguments works: value vs. reference
+
+const flight = 'LH234';
+const aminul = {
+  name: 'Aminul Islam',
+  passport: 2493042039,
 };
-console.log(bookings);
-createBooking('LH123', undefined, 800);
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = `Mr ${passenger.name}`;
+  if (passenger.passport === 2493042039) {
+    alert('Checked In');
+  } else {
+    alert('Wrong passport');
+  }
+};
+
+checkIn(flight, aminul);
+console.log(flight);
+console.log(aminul);
+
+const flightNum = flight;
+const passenger = aminul;
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 10000000);
+};
+newPassport(aminul);
+checkIn(flight, aminul);
