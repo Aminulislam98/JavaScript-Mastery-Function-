6c5d8cc91ae382d2df4d3a4fa3e97b252dc94047
Aminul islam <aminurrohman98@gmail.com>
2026-07-02 21:44:48 +0100

    Learned about regex pattern , learned how it works and used it

--- script.js
+++ script.js
@@ -53,3 +53,21 @@
 // In JavaScript : Functions are objects
 // Learn about Higher order function
 // Function accepting callback function functions
+
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+// Higher-order Function
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed strings: ${fn(str)}`);
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer('JavaScript is the best!', upperFirstWord);
+transformer('JavaScript is the best!', oneWord);
